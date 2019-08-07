48860406f9b712f38635faff4a1ac6e379a4866a
Elinaldo Monteiro <elinaldo.java@gmail.com>
2019-08-07 13:35:23 -0300

    Add several resolution of same problem

--- 01/main.py
+++ 01/main.py
@@ -1,12 +1,13 @@
-def countApplesAndOranges(s: int, t: int, a: int, b: int, apples: list, oranges: list):
-    """
-        s: integer, starting point of Sam's house location.
-        t: integer, ending location of Sam's house location.
-        a: integer, location of the Apple tree.
-        b: integer, location of the Orange tree.
-        apples: integer array, distances at which each apple falls from the tree.
-        oranges: integer array, distances at which each orange falls from the tree.
-    """
+"""
+s: integer, starting point of Sam's house location.
+t: integer, ending location of Sam's house location.
+a: integer, location of the Apple tree.
+b: integer, location of the Orange tree.
+apples: integer array, distances at which each apple falls from the tree.
+oranges: integer array, distances at which each orange falls from the tree.
+"""
+
+def count_apples_and_oranges_01(s: int, t: int, a: int, b: int, apples: list, oranges: list):
     interval = list(range(s, t + 1))
     qty_apples = 0
     qty_oranges = 0
@@ -20,5 +21,24 @@ def countApplesAndOranges(s: int, t: int, a: int, b: int, apples: list, oranges:
         orange += b
         if orange in interval:
             qty_oranges += 1
-
     return qty_apples, qty_oranges
+
+def count_apples_and_oranges_02(s: int, t: int, a: int, b: int, apples: list, oranges: list):
+    qty_apples = 0
+    qty_oranges = 0
+    while apples or oranges:
+        if apples:
+            apple = apples.pop() + a
+            if t >= apple >= s:
+                qty_apples += 1
+        if oranges:
+            orange = oranges.pop() + b
+            if t >= orange >= s:
+                qty_oranges += 1
+    return qty_apples, qty_oranges
+
+def count_apples_and_oranges_03(s: int, t: int, a: int, b: int, apples: list, oranges: list):
+    return sum([1 for apple in apples if t >= apple + a >= s]), sum([1 for orange in oranges if t >= orange + b >= s])
+
+def count_apples_and_oranges_04(s: int, t: int, a: int, b: int, apples: list, oranges: list):
+    return sum(t >= apple + a >= s for apple in apples), sum(t >= orange + b >= s for orange in oranges)
--- 01/main_test.py
+++ 01/main_test.py
@@ -1,34 +1,72 @@
-import main
+import copy
+from main import (
+    count_apples_and_oranges_01,
+    count_apples_and_oranges_02,
+    count_apples_and_oranges_03,
+    count_apples_and_oranges_04,
+)
 
 
 class TestMain:
+    def setup(self):
+        self.fixtures = [
+            {
+                "s": 7,
+                "t": 10,
+                "a": 4,
+                "b": 12,
+                "apples": [2, 3, -4],
+                "oranges": [3, -2, -4],
+            },
+            {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [-2], "oranges": [-1]},
+            {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [2], "oranges": [-2]},
+            {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [2, 2], "oranges": [-2, -2]},
+        ]
+
     def test_01(self):
-        data = {
-            "s": 7,
-            "t": 10,
-            "a": 4,
-            "b": 12,
-            "apples": [2, 3, -4],
-            "oranges": [3, -2, -4],
-        }
-        apples, oranges = main.countApplesAndOranges(**data)
-        assert apples == 1
-        assert oranges == 2
+        for func in [
+            count_apples_and_oranges_01,
+            count_apples_and_oranges_02,
+            count_apples_and_oranges_03,
+            count_apples_and_oranges_04,
+        ]:
+            values = copy.deepcopy(self.fixtures[0])
+            apples, oranges = func(**values)
+            assert apples == 1
+            assert oranges == 2
 
     def test_02(self):
-        data = {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [-2], "oranges": [-1]}
-        apples, oranges = main.countApplesAndOranges(**data)
-        assert apples == 0
-        assert oranges == 0
+        for func in [
+            count_apples_and_oranges_01,
+            count_apples_and_oranges_02,
+            count_apples_and_oranges_03,
+            count_apples_and_oranges_04,
+        ]:
+            values = copy.deepcopy(self.fixtures[1])
+            apples, oranges = func(**values)
+            assert apples == 0
+            assert oranges == 0
 
     def test_03(self):
-        data = {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [2], "oranges": [-2]}
-        apples, oranges = main.countApplesAndOranges(**data)
-        assert apples == 1
-        assert oranges == 1
+        for func in [
+            count_apples_and_oranges_01,
+            count_apples_and_oranges_02,
+            count_apples_and_oranges_03,
+            count_apples_and_oranges_04,
+        ]:
+            values = copy.deepcopy(self.fixtures[2])
+            apples, oranges = func(**values)
+            assert apples == 1
+            assert oranges == 1
 
     def test_04(self):
-        data = {"s": 2, "t": 3, "a": 1, "b": 5, "apples": [2, 2], "oranges": [-2, -2]}
-        apples, oranges = main.countApplesAndOranges(**data)
-        assert apples == 2
-        assert oranges == 2
+        for func in [
+            count_apples_and_oranges_01,
+            count_apples_and_oranges_02,
+            count_apples_and_oranges_03,
+            count_apples_and_oranges_04,
+        ]:
+            values = copy.deepcopy(self.fixtures[3])
+            apples, oranges = func(**values)
+            assert apples == 2
+            assert oranges == 2
